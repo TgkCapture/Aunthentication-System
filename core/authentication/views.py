1668791ad97eb68cc11ee3b111f48cecb22830dd
tgkcapture <71700867+TgkCapture@users.noreply.github.com>
2022-08-06 03:48:35 +0200

    main views

--- core/authentication/views.py
+++ core/authentication/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+def home(request):
+    return render(request, 'authentication/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        fname = request.POST['firstName']
+        lname = request.POST['lastName']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['confirm_password']
+
+        myuser = User.objects.create_user(username, email, password)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(request, "Your account has been successfully created")
+        return redirect('/login')
+
+
+    return render(request, 'authentication/register.html')
+
+def login(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You are now logged in")
+            return render(request, 'authentication/index.html', {'username': username})
+        else:
+            messages.error(request, "Invalid Credentials")
+            return redirect('/login')
+
+
+    return render(request, 'authentication/login.html')
+
+def logout(request):
+    pass 
