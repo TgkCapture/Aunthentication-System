e6e44b3ce47b15c10550be5f69e29a512c2f83e9
tgkcapture <71700867+TgkCapture@users.noreply.github.com>
2022-08-06 15:33:33 +0200

    additions

--- core/core/info.py
+++ core/core/info.py
@@ -1,5 +1,7 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'proxydemo2021@gmail.com'
+EMAIL_HOST_USER = 'youremailemail.com'
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_FILE_PATH = './templates/emails'
