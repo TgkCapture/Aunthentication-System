68e62c5842e2730ebad74f98f666ea59f9156a1a
tgkcapture <71700867+TgkCapture@users.noreply.github.com>
2022-08-06 15:30:38 +0200

    email verification function

--- core/authentication/views.py
+++ core/authentication/views.py
@@ -4,7 +4,12 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from core import settings
 from django.core.mail import send_mail
-
+from django.core.mail import EmailMessage
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from . tokens import account_activation_token
 
 
 
@@ -37,6 +42,7 @@ def register(request):
             return redirect('home')
         else:
             myuser = User.objects.create_user(username=username, first_name=firstName, last_name=lastName, email=email, password=password)
+            myuser.is_active = False
             myuser.save()
             messages.success(request, "You are now registered , verify your email to login")
 
@@ -47,6 +53,23 @@ def register(request):
             recipient_list = [myuser.email]
             send_mail( subject, message, email_from, recipient_list, fail_silently=False )
 
+            # Verification Email
+            current_site = get_current_site(request)
+            email_subject = 'Activate Your Django Authentication Account'
+            email_message = render_to_string('acc_active_email.html', {
+                'name': myuser.first_name,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+                'token': account_activation_token.make_token(myuser),
+            })
+            email = EmailMessage(
+                email_subject,
+                email_message,
+                settings.EMAIL_HOST_USER,
+                [myuser.email],
+            )
+            email.fail_silently = False
+            email.send()          
 
             return redirect('login')
 
@@ -87,3 +110,17 @@ def logout(request):
     auth_logout(request)
     messages.success(request, "You are now logged out") 
     return redirect('home')
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        myuser = None
+    if myuser is not None and account_activation_token.check_token(myuser, token):
+        myuser.is_active = True
+        myuser.save()
+        auth_login(request, myuser)
+        return redirect('home')
+    else:
+        return render(request, 'activation_failed.html')
